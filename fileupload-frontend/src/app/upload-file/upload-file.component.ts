941aa80c0ddfd33a0b83c91ef03d81311060ea96
Shiv Darshan <shivroy121@gmail.com>
2023-03-25 17:39:37 +0530

    fix  Scatter plot

--- fileupload-frontend/src/app/upload-file/upload-file.component.ts
+++ fileupload-frontend/src/app/upload-file/upload-file.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FileDetails } from '../file-details.model';
 import { FileUploadService } from '../services/file-upload.service';
+import { AgChartOptions } from 'ag-charts-community';
 
 @Component({
   selector: 'app-upload-file',
@@ -11,10 +12,17 @@ import { FileUploadService } from '../services/file-upload.service';
 export class UploadFileComponent implements OnInit {
 
   file!: File;
-  fileDetails!: FileDetails;
-  fileUris: Array<string> = [];
+  fileDetails: FileDetails[]=[];
+  
 
-  constructor(private fileUploadService: FileUploadService, private router: Router) { }
+  public options!: AgChartOptions;
+
+  xval: any[] = [];
+
+
+  constructor(private fileUploadService: FileUploadService, private router: Router) { 
+    this.setOption(this.xval);
+  }
 
   ngOnInit(): void {
   }
@@ -26,14 +34,84 @@ export class UploadFileComponent implements OnInit {
   uploadFile() {
     this.fileUploadService.upload(this.file).subscribe({
       next: (data) => {
-        this.fileDetails = data;
-        this.fileUris.push(this.fileDetails.fileUri);
+        this.fileDetails=data;
+
+         let x:any[]=[];
+        let y: any[] = [];
+       
+        for (let i = 0; i < data.length; i++) {
+
+          let jsonX = { x: this.fileDetails[i].x, y: this.fileDetails[i].y };
+         
+         
+          x.push(jsonX);
+          
+         
+        }
+
+        this.setOption(x);
+       
         alert("File Uploaded Successfully")
       },
       error: (e) => {
         console.log(e);
       }
     });
+  }
+
+  setOption(data:any) {
+    
+    this.options = {
+      autoSize: true,
+      title: {
+        text: 'X vs Y',
+      },
+      series: [
+        {
+          type: 'scatter',
+          title: 'X',
+          data: data,
+          xKey: 'x',
+          xName: 'X',
+          yKey: 'y',
+          yName: 'Y',
+          marker: {
+            shape: 'square',
+            size: 6,
+            maxSize: 30,
+            fill: 'rgba(227,111,106,0.71)',
+            stroke: '#9f4e4a',
+          },
+        }
+      ],
+      axes: [
+        {
+          type: 'number',
+          position: 'bottom',
+          title: {
+            text: 'X',
+          },
+          label: {
+            rotation: 45,
+            formatter: (params) => {
+              return params.value + 'cm';
+            },
+          },
+        },
+        {
+          type: 'number',
+          position: 'left',
+          title: {
+            text: 'Y',
+          },
+          label: {
+            formatter: (params) => {
+              return params.value + 'kg';
+            },
+          },
+        },
+      ],
+    };
   }
 
 }
